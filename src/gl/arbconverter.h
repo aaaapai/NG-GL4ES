67088e447ae2d834d4cfa159d1532e4f32ce486d
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-30 19:08:28 +0800

    Add files via upload

--- src/gl/arbconverter.h
+++ src/gl/arbconverter.h
@@ -1,8 +1,17 @@
 #ifndef _GL4ES_ARBCONVERTER_H_
 #define _GL4ES_ARBCONVERTER_H_
 
-#include <stdint.h>
+#include <cstdint>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 char* gl4es_convertARB(const char* const code, int vertex, char **error_msg, int *error_ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif // _GL4ES_ARBCONVERTER_H_
