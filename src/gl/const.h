71dfd1d0ba62d6a68b4dbe9fb3cd4541d94ab8ab
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-14 22:28:20 +0800

    Update const.h

--- src/gl/const.h
+++ src/gl/const.h
@@ -11,6 +11,34 @@ extern "C" {
 #define GL_LUMINANCE_ALPHA16F          0x881F
 #define GL_LUMINANCE_ALPHA32F          0x8819
 
+#define GL_ALPHA4				       0x803B
+#define GL_ALPHA8				       0x803C
+#define GL_ALPHA12				       0x803D
+#define GL_ALPHA16				       0x803E
+#define GL_ALPHA16F                    0x881C
+#define GL_ALPHA32F                    0x8816
+#define GL_LUMINANCE4				   0x803F
+#define GL_LUMINANCE8				   0x8040
+#define GL_LUMINANCE12				   0x8041
+#define GL_LUMINANCE16				   0x8042
+#define GL_LUMINANCE16F                0x881E
+#define GL_LUMINANCE32F                0x8818
+#define GL_LUMINANCE4_ALPHA4		   0x8043
+#define GL_LUMINANCE6_ALPHA2		   0x8044
+#define GL_LUMINANCE8_ALPHA8		   0x8045
+#define GL_LUMINANCE12_ALPHA4		   0x8046
+#define GL_LUMINANCE12_ALPHA12		   0x8047
+#define GL_LUMINANCE16_ALPHA16		   0x8048
+#define GL_LUMINANCE_ALPHA16F          0x881F
+#define GL_LUMINANCE_ALPHA32F          0x8819
+#define GL_INTENSITY				   0x8049
+#define GL_INTENSITY4				   0x804A
+#define GL_INTENSITY8				   0x804B
+#define GL_INTENSITY12				   0x804C
+#define GL_INTENSITY16				   0x804D
+#define GL_INTENSITY16F                0x881D
+#define GL_INTENSITY32F                0x8817
+
 #include <GL/gl.h>
 #include <GL/glext.h>
 
