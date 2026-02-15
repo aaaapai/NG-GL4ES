5fcc90746a8829fbff749fe49a6815d68abc9dac
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-15 21:36:16 +0800

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -5,8 +5,8 @@
 # git clone --depth 1 https://github.com/Cyan4973/xxHash.git ./3rdparty/xxHash
 cmake_build () {
   ANDROID_ABI=$1
-  mkdir -p build
-  cd build
+  mkdir -p $ANDROID_ABI/build
+  cd $ANDROID_ABI/build
   cmake $GITHUB_WORKSPACE -DANDROID_PLATFORM=26 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DThreads_FOUND=ON -DCMAKE_THREAD_LIBS_INIT="-pthread" -DCMAKE_USE_PTHREADS_INIT=ON -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
   cmake --build . --config Release --parallel 6
   # 在bash中启用globstar
@@ -16,3 +16,5 @@ cmake_build () {
 
 cmake_build arm64-v8a
 cmake_build x86_64
+cmake_build x86
+cmake_build armeabi-v7a
